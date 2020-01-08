ee10ab8bb0083e742c5706735c42a36fae1c458a
uta8a <noumodulity@gmail.com>
2020-01-09 03:13:56 +0900

    add: rust example test

---
+++ src/rust/example/main.rs
@@ -0,0 +1,6 @@
+// name   : kaito_tateyama (uta8a)
+// tag    : "tag"
+// comment: ""
+fn add_two(a: i32) -> i32 {
+    a + 2
+}
---
+++ test/rust/example/main.rs
@@ -0,0 +1,8 @@
+// name   : kaito_tateyama (uta8a)
+// tag    : "tag"
+// comment: ""
+#[test]
+fn test_add_two() {
+    assert_eq!(add_two(2), 4);
+}
+
