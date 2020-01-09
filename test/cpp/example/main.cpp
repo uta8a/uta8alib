28796fd63860456bc2d7a3d37efb2d64c84cab97
uta8a <noumodulity@gmail.com>
2020-01-09 15:43:45 +0900

    add: test example

--- test/cpp/example/main.cpp
+++ test/cpp/example/main.cpp
@@ -1,3 +1,11 @@
 // name   : kaito_tateyama (uta8a)
 // tag    : "tag"
 // comment: ""
+
+#include <catch2/catch.hpp>
+#include <../example/main.hpp>
+#include <bits/stdc++.h>
+
+TEST_CASE( "Example" ) {
+    REQUIRE(add_two(2) == 4);
+}
